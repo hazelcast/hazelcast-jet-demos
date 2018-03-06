45260fdd1f7028986260670a00ee089142439253
Emin Demirci <emin@hazelcast.com>
2018-03-06 12:02:50 +0300

    Added Core API version of the cryptocurrency-realtime-trend demo (#58)

---
+++ cryptocurrency-realtime-trend/src/main/java/com/hazelcast/jet/demo/core/SentimentProcessor.java
@@ -0,0 +1,38 @@
+package com.hazelcast.jet.demo.core;
+
+import com.hazelcast.jet.Traversers;
+import com.hazelcast.jet.core.AbstractProcessor;
+import com.hazelcast.jet.core.ResettableSingletonTraverser;
+import com.hazelcast.jet.datamodel.TimestampedEntry;
+import com.hazelcast.jet.demo.common.SentimentAnalyzer;
+import edu.stanford.nlp.util.CoreMap;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+import static java.lang.Double.isInfinite;
+import static java.lang.Double.isNaN;
+
+public class SentimentProcessor extends AbstractProcessor {
+
+    private final SentimentAnalyzer analyzer = new SentimentAnalyzer();
+    private final ResettableSingletonTraverser<TimestampedEntry<String, Double>> traverser = new
+            ResettableSingletonTraverser<>();
+    private final FlatMapper<TimestampedEntry<String, String>, TimestampedEntry<String, Double>> mapper =
+            flatMapper(e -> {
+                List<CoreMap> annotations = analyzer.getAnnotations(e.getValue());
+                double sentimentType = analyzer.getSentimentClass(annotations);
+                double sentimentScore = analyzer.getScore(annotations, sentimentType);
+
+                double score = sentimentType * sentimentScore;
+                if (isNaN(score) || isInfinite(score)) {
+                    return Traversers.empty();
+                }
+                traverser.accept(new TimestampedEntry<>(e.getTimestamp(), e.getKey(), score));
+                return traverser;
+            });
+
+    @Override
+    protected boolean tryProcess(int ordinal, @Nonnull Object item) {
+        return mapper.tryProcess(((TimestampedEntry<String, String>) item));
+    }
+}
