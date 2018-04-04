69919d7a661fecfc98570a4775dec8f554987811
Can Gencer <cgencer@gmail.com>
2018-04-04 19:02:45 +0200

    Rename modules

---
+++ market-data-ingest/hazelcast-grid/src/main/java/com/hazelcast/jet/demos/market/data/ingest/PreciousHistory.java
@@ -0,0 +1,24 @@
+package com.hazelcast.jet.demos.market.data.ingest;
+
+import com.hazelcast.jet.pipeline.JournalInitialPosition;
+import com.hazelcast.jet.pipeline.Pipeline;
+import com.hazelcast.jet.pipeline.Sinks;
+import com.hazelcast.jet.pipeline.Sources;
+
+public class PreciousHistory {
+
+    public static Pipeline build() {
+        Pipeline p = Pipeline.create();
+
+        // Palladium and Platinum only
+        p.drawFrom(Sources.<String, Object>mapJournal(
+                Constants.IMAP_NAME_PRECIOUS, JournalInitialPosition.START_FROM_OLDEST)
+        ).map(e -> e.getKey() + "==" + e.getValue())
+         .filter(str -> str.toLowerCase().startsWith("p"))
+         .drainTo(Sinks.logger())
+        ;
+
+        return p;
+    }
+
+}
