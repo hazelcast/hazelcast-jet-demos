e2b2c29413df29c5455c01c55bc32a40a08f2b08
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-03-06 22:28:54 +0000

    Initial commit

---
+++ bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/Task3.java
@@ -0,0 +1,47 @@
+package com.hazelcast.jet.demos.bitcoin;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ITopic;
+import com.hazelcast.jet.demos.bitcoin.alerting.MyTopicListener;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * <p>This is {@code Task3}. The use of the "{@code @Order}"
+ * annotation tells Spring the order to run these tasks.
+ * </p>
+ * <p>{@code Task3} adds a broadcast listener on a 
+ * {@link com.hazelcast.core.ITopic ITopic}, so all members
+ * in the cluster are alerted if the price of Bitcoin
+ * does something interesting.
+ * </p>
+ * <p><b>Note:</b> {@code Task1}, {@code Task2} and {@code Task3}
+ * are ordered to run before {@code Task4}. The first three can
+ * be run in any order, all will appear to do nothing until the
+ * fourth starts producing data.
+ * </p>
+ */
+@Component
+@Order(MyConstants.PRIORITY_THREE)
+@Slf4j
+public class Task3 implements CommandLineRunner {
+
+	@Autowired
+	private HazelcastInstance hazelcastInstance;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("{} - Start topic listener", this.getClass().getSimpleName());
+
+		ITopic<Object> iTopic =
+				this.hazelcastInstance
+				.getTopic(MyConstants.ITOPIC_NAME_ALERT);
+		
+		iTopic.addMessageListener(new MyTopicListener());
+	}
+}
--- bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/domain/Price.java
+++ bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/domain/Price.java
@@ -3,6 +3,7 @@ package com.hazelcast.jet.demos.bitcoin.domain;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import lombok.Data;
 
@@ -28,5 +29,16 @@ public class Price implements Comparable<Price>, Serializable {
 	public int compareTo(Price that) {
 		return this.localDate.compareTo(that.getLocalDate());
 	}
+
+	/**
+	 * <p>Turn the date into a number of days, relative to
+	 * some point in time. We use {@code LocalDate.now()} which
+	 * is good enough if we don't run this demo over midnight.
+	 * </p>
+	 * @return A non-zero positive number, all prices are historical
+	 */
+    public long getDayCounter() {
+        return ChronoUnit.DAYS.between(this.localDate, LocalDate.now());
+    }
 
 }
