6e593267c94cc3f25c655d769c62ebbef5f040c4
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-04-17 07:57:56 +0100

    Initial commit

--- bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/job/MovingAverage.java
+++ bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/job/MovingAverage.java
@@ -27,7 +27,8 @@ import com.hazelcast.jet.pipeline.WindowDefinition;
 
 /**
  * <p>Creates a processing pipeline to calculate moving
- * averages and detect when they cross.
+ * averages and detect when they cross. This is invoked
+ * by {@link Task1}.
  * </p>
  * <p>This is sequential, the output from one step only
  * goes to subsequent steps. There are no loops, as this
@@ -40,33 +41,130 @@ import com.hazelcast.jet.pipeline.WindowDefinition;
  * <p>The processing looks like this:
  * </p>
  * <pre>
- * XXX
+ *                         +-------------------+
+ *                         |     priceFeed     |
+ *                         |  averageOf1-noop  |
+ *                         |    averageOf50    |
+ *                         |    averageOf200   |
+ *                         |      logSink      |
+ *                         | mapSink-50 Point  |
+ *                         | mapSink-200 Point |
+ *                         |  mapSink-Current  |
+ *                         |    streamOf50     |
+ *                         |    streamOf200    |
+ *                         |      joined       |
+ *                         |     reformat      |
+ *                         |   crossEmitter    |
+ *                         |  topicSink-alert  |
+ *                         +-------------------+
+ * +-------------------+   +-------------------+   +-------------------+
+ * +-------------------+   +-------------------+   +-------------------+
+ * XXX Diagram goes here
  * </pre>
  * <p>What each step does is as follows:
  * </p>
  * <ol>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>priceFeed</b> : {@link Task4} writes Bitcoin / US Dollar
+ * prices into an {@code com.hazelcast.core.IMap IMap}. Each new price
+ * replaces the previous, so what is stored in Hazelcast in the
+ * "{@code prices-in}" map is the current price.
+ * </p>
+ * <p>However, Hazelcast is configured (see "{@code hazelcast.xml}") to
+ * be able to create an input stream of <em>changes</em>. Each time the
+ * entry is updated in the map, Hazelcast stores the new value and
+ * produces an event on this infinite stream.
+ * </p>
+ * <p>Each price in the input has a date, but we ignore this date on
+ * the input and regard the stream as having no timestamps as we
+ * don't need this timestamp yet and when we derive a value from
+ * this input it will have a different timestamp.
+ * </li>
+ * <li><p><b>averageOf50</b> : Produce a moving average of the last
+ * <em>50</em> prices. Each input is added to a ring-buffer of size
+ * 50. Once the ring-buffer is full, add the 50 prices up, divide
+ * by 50 and produce the first output item. Thereafter each item
+ * of input results in one item of output.
+ * </p>
+ * <p>If the ring-buffer was size 3, the first item of output
+ * would be the average of input items 1, 2 and 3. The second item
+ * of output would be the average of items 2, 3 and 4.
+ * <p>
+ * </li>
+ * <li><p><b>averageOf200</b> : This is the same as the "{@code averageOf50}"
+ * stage, except the parameter is 200 to produce the moving average of
+ * the last 200 prices.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>averageOf1-noop</b> : Calculate the moving average of
+ * using a ring-buffer size of 1. Mathematically this does nothing
+ * (a "{@code no-op}"), the average of one number is that number. We
+ * use it here as a convenience to reformat the raw input into the
+ * same format as produced by the "{@code averageOf50}" and
+ * "{@code averageOf200}" streams so we can treat all three the
+ * same later on.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>logSink</b> : <i>[Optional] : </i> Write the output
+ * of the "{@code averageOf1-noop}" stage to the console. This
+ * is just so we can see what is happening with the prices, to
+ * aid understanding.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>mapSink-50 Point</b> : Save the "{@link averageOf50}"
+ * stream to an {@link com.hazelcast.core.IMap IMap} named 
+ * "{@code BTCUSD}". {@link Task2} listens to this map to capture
+ * the values to plot on the graph. Each value replaces the previous
+ * in the map, so this map holds the most recent 50-point average.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>mapSink-200 Point</b> : Save the stream from
+ * "{@code averageOf200}" into the same map as we save the
+ * "{@code averageOf50}". So {@link Task2} can track this value
+ * too to plot on the graph, and this map stores the last
+ * 200-point average.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>mapSink-Current</b> : Save the stream of unaltered
+ * prices into the map named "{@code BTCUSD}". This is primarily
+ * for {@link Task2}.</p>
+ * <p>So map "{@code BTCUSD}" holds the latest values for the
+ * 50-point average, 200-point average and unaltered original
+ * price conveniently together. The unaltered price came from
+ * and still exists in another map "{@code prices-in}" so
+ * this is minor waste of space to duplicate it.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>streamOf50</b> : prepare the "{@code averageOf50}"
+ * stream for joining.
+ * </p>
+ * <p>Firstly, add a timestamp. For the 50-point average we
+ * could take the date of the first of the 50-points, the last
+ * of the 50-points or the middle. All would have a sense to
+ * them. We take the date of the last of the 50-points.
+ * </p>
+ * <p>Apply a window to this stream so Jet will control the release
+ * of values to the next stage. We chose a tumbling window as
+ * we don't want the next stage to see each average more than
+ * once. We advance the window in 1-day units, and since we have
+ * 1 average per day, this means our window contains at most one
+ * value.
  * </p>
  * </li>
- * <li><p><b>??</b> : XXX
+ * <li><p><b>streamOf200</b> : prepare the "{@code averageOf200}"
+ * stream for joining by adding a timestamp to join on. Again,
+ * use the end date of the average range as the timestamp.
+ * </p>
+ * </li>
+ * <li><p><b>joined</b> : XXX
+ * </p>
+ * </li>
+ * <li><p><b>reformat</b> : XXX
+ * </p>
+ * </li>
+ * <li><p><b>crossEmitter</b> : XXX
+ * </p>
+ * </li>
+ * <li><p><b>topicSink-alert</b> : XXX
  * </p>
  * </li>
  * </ol>
@@ -133,8 +231,8 @@ public class MovingAverage {
 			.drainTo(Sinks.logger())
 			.setName("logSink");
 		
-		/** <p><i>Optional: </i>Save the latest for each average
-		 * to an {@link com.hazelcast.core.IMap IMap}.</p>
+		/** <p>Save the latest for each average to an
+		 * {@link com.hazelcast.core.IMap IMap} for {@link Task2}.</p>
 		 */
 		averageOf1
 			.drainTo(Sinks.map(MyConstants.IMAP_NAME_PRICES_OUT_BTCUSD))
@@ -146,42 +244,44 @@ public class MovingAverage {
 			.drainTo(Sinks.map(MyConstants.IMAP_NAME_PRICES_OUT_BTCUSD))
 			.setName("mapSink-" + MyConstants.KEY_200_POINT);
 
-		/** <p>Create a "cursor" for joining the 50-point price stream.
-		 * Advance it 1 day, and therefore 1 point, at a time.
+		/** <p>Create a timestamped stream from the 50-point averages.
+		 * Add a window that advances it 1 day, and therefore 1 point,
+		 * at a time.
 		 * </p>
 		 */
-		StageWithKeyAndWindow<Entry<String, Price>, String> windowOf50 =
-				MovingAverage.buildWindow(averageOf50, 50)
+		StageWithKeyAndWindow<Entry<String, Price>, String> timestampedOf50 =
+				MovingAverage.buildKeyedTimestamped(averageOf50, 50)
 				.window(ONE_DAY_WINDOW);
 		
-		/** <p>Create a "cursor" for joining the 200-point price stream.
+		/** <p>Create a timestamped stream from the 200-point averages.
 		 * </p>
 		 */
-		StreamStageWithKey<Entry<String, Price>, String> windowOf200 =
-				MovingAverage.buildWindow(averageOf200, 200);
+		StreamStageWithKey<Entry<String, Price>, String> timestampedOf200 =
+				MovingAverage.buildKeyedTimestamped(averageOf200, 200);
 		
-		/** <p>Join the cursors on the 50-point and 200-point streams,
-		 * producing output when both sides have a value for the same
-		 * date.
+		/** <p>Join the timestamped 50-point and 200-point streams by
+		 * the timestamped. Produces a trio of the date, and the
+		 * price from each input for the matching date.
 		 * </p>
 		 */
 		StreamStageWithKey<SimpleImmutableEntry<String, Tuple3<LocalDate, BigDecimal, BigDecimal>>, String> 
 			joined50point200point
-			= MovingAverage.join(windowOf50,windowOf200);
+			= MovingAverage.join(timestampedOf50,timestampedOf200);
 			
-		/** <p>The {@link CrossEmitter} takes matching pairs of
-		 * 50-point and 200-point averages, and produces output
-		 * only if this has reversed from the previous day.
+		/** <p>Business logic!</p>
+		 * <p>Compare the trio of date, 50-point and 200-point in the
+		 * stream to the previous, looking for price inflection.
 		 * </p>
 		 */
 		StreamStage<Entry<?,?>> alerts =
 			joined50point200point.customTransform("crossEmitter", CrossEmitter::new);
 
 		/** <p>If there is anything produced by the {@link CrossEmitter}
-		 * dump it to a {@link com.hazelcast.core.ITopic ITopic}. What
-		 * comes out is a map entry so we could easily dump it to a
-		 * {@link com.hazelcast.core.IMap IMap} instead (or as well)
-		 * and use a map listener.
+		 * dump it to a {@link com.hazelcast.core.ITopic ITopic} for
+		 * {@link Task3}. What comes out is a {@code Map.Entry} so we could
+		 * easily dump it to an {@link com.hazelcast.core.IMap IMap} instead
+		 * (or as well) and use a map listener.
+		 * </p>
 		 */
 		alerts.drainTo(MovingAverage.buildAlertSink());
 		
@@ -255,19 +355,19 @@ public class MovingAverage {
 	
 	/**
 	 * <p>Take a stream of prices that happen to be a derived (average)
-	 * stream of prices, and create a window on that stream. Based the window
-	 * on the timestamp which is the last date of the average.
+	 * stream of prices, and create a timestamp on that stream. Based the
+	 * timestamp on the last date of the average.
 	 * </p>
 	 *
 	 * @param averageOfSomething Average of 50 points or the average of 200 points
 	 * @param count For building the stage name
-	 * @return A window on this input
+	 * @return A timestamped stream on this input
 	 */
 	private static StreamStageWithKey<Entry<String, Price>, String> 
-		buildWindow(StreamStage<Entry<String, Price>> averageOfSomething, int count) {
+		buildKeyedTimestamped(StreamStage<Entry<String, Price>> averageOfSomething, int count) {
 		return averageOfSomething
 				.addTimestamps(e -> e.getValue().getTimestamp(), ZERO_LAG)
-				.setName("windowOf" + count)
+				.setName("streamOf" + count)
 				.groupingKey(MovingAverage.whence())
 				;
 	}
