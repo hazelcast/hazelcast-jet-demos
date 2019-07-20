59946c2ad348e4e28c01a3055c6629a2ee72ac8f
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-07-20 12:26:51 +0100

    Junit

--- train-track/train-track-beam-job/src/test/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFnTest.java
+++ train-track/train-track-beam-job/src/test/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFnTest.java
@@ -1,40 +1,51 @@
 package com.hazelcast.jet.demos.traintrack;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MyEnrichAndReformatFnTest {
 
+	static final long NOW = 987654321;
+	
+	static MyEnrichAndReformatFn myEnrichAndReformatFn;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		//XXX
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		//XXX
-	}
-
-	@Before
-	public void setUp() throws Exception {
-		//XXX
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		//XXX
+		myEnrichAndReformatFn = new MyEnrichAndReformatFn(NOW);
 	}
 
 	@Test
-	public void test() {
-		String input = "123,456,789";
-
-		fail("Not yet implemented");
+	public void json_parse() throws JSONException {
+		long offset = 789;
+		String input = "123,456," + offset;
+		
+		String output = myEnrichAndReformatFn._process(input);
+		
+		log.info("{} -> {}", input, output);
+		
+		assertThat("output", output, not(nullValue()));
+		
+		JSONObject jsonObject = new JSONObject(output);
+		
+		String latitude = jsonObject.getString("latitude");
+		assertThat("latitude", latitude, not(nullValue()));
+		assertThat("latitude", latitude, is(equalTo("123")));
+		
+		String longitude = jsonObject.getString("longitude");
+		assertThat("longitude", longitude, not(nullValue()));
+		assertThat("longitude", longitude, is(equalTo("456")));
+		
+		String timestamp = jsonObject.getString("timestamp");
+		assertThat("timestamp", timestamp, not(nullValue()));
+		assertThat("timestamp", timestamp, is(equalTo(String.valueOf(NOW + offset * 1000))));
 	}
 
 }
