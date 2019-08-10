4a97198e15ee662a1fa9ef9431761ec9675ea033
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-08-10 20:30:19 +0100

    Output with timestamp

--- train-track/train-track-beam-job/src/main/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFn.java
+++ train-track/train-track-beam-job/src/main/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFn.java
@@ -1,6 +1,10 @@
 package com.hazelcast.jet.demos.traintrack;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.apache.beam.sdk.transforms.DoFn;
+import org.joda.time.Instant;
 
 /**
  * <p>The input element of the process context is a CSV string.
@@ -14,43 +18,89 @@ import org.apache.beam.sdk.transforms.DoFn;
 @SuppressWarnings("serial")
 public class MyEnrichAndReformatFn extends DoFn<String, String> {
 
-	private final long startTimestamp;
-
-	MyEnrichAndReformatFn(long arg0) {
+	private static final SimpleDateFormat simpleDateFormat 
+		= new SimpleDateFormat("HH:mm:ss");
+	
+	private long count;
+	private long startTimestamp;
+	
+	public MyEnrichAndReformatFn() {
+		this(0);
+	}
+	public MyEnrichAndReformatFn(long arg0) {
 		this.startTimestamp = arg0;
 	}
 
 	/**
-	 * <p>Use a helper function to take the input from the
+	 * <p>Use helper functions to take the input from the
 	 * execution context to make the output.
 	 * </p>
 	 *
-	 * @param processContext
+	 * @param element String from the ProcessContext
+	 * @param receiver Where to send the processed input
 	 */
     @ProcessElement
-    public void processElement(ProcessContext processContext) { 
-    	processContext.output(this._process(processContext.element()));
+    public void processElement(@Element String element, OutputReceiver<String> receiver) { 
+    	if (this.startTimestamp==0) {
+    		this.startTimestamp = System.currentTimeMillis();
+    	}
+    	this.count++;
+    	
+    	long offset = _getOffset(element) * MyBeamJob.QUARTER_OF_A_SECOND_MS;
+    	
+    	long timestamp = _calculateTimestamp(offset);
+
+    	String result = _makeJson(element, timestamp);
+
+        receiver.outputWithTimestamp(result, new Instant(timestamp));
 	}
 
+    
     /**
-     * <p>Separate out the reformatting from execution context access
-     * for easier unit testing.
+     * <p>Find field 3 in CSV. Very slim need for this to be a
+     * separate method.
+     * </p>
+     * 
+     * @param input Something comma something comma offset
+     * @return The offset
+     */
+    public long _getOffset(String input) {
+    	String[] trio = input.split(",");
+
+    	return Long.parseLong(trio[2]);
+    }
+    
+    /**
+     * <p>Add the offset of the time in the original input data to
+     * the start time, to make the effective time.
+     * </p>
+     * 
+     * @param offset From GPS
+     * @return
+     */
+    public long _calculateTimestamp(long offset) {
+    	return this.startTimestamp + offset;
+    }
+    
+    /**
+     * <p>Reformat the input into Json.
      * </p>
      *
      * @param input String in CSV form
+     * @param timestamp Timestamp to use to appear it is happening now
      * @return String in JSON form
      */
-    public String _process(String input) {
+    public String _makeJson(String input, long timestamp) {
     	String[] trio = input.split(",");
     	
         StringBuilder sb = new StringBuilder("{");
-
+        
+        sb.append(" \"input-point\": \"" + this.count + "\",");
+        
         sb.append(" \"latitude\": \"" + trio[0] + "\",");
         sb.append(" \"longitude\": \"" + trio[1] + "\",");
-
-        // Field 3 is offset in seconds.
-        long timestamp = this.startTimestamp + 1000 * Long.parseLong(trio[2]);
-        sb.append(" \"timestamp\": \"" + timestamp + "\"");
+        sb.append(" \"timestamp\": \"" + timestamp + "\",");
+        sb.append(" \"timeprint\": \"" + simpleDateFormat.format(new Date(timestamp)) + "\"");
 
         sb.append(" }");
         
