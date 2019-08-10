b66b88282df9cf87fc40adf34e3d72d40d74af76
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-08-10 14:24:09 +0100

    Test

--- train-track/train-track-beam-job/src/test/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFnTest.java
+++ train-track/train-track-beam-job/src/test/java/com/hazelcast/jet/demos/traintrack/MyEnrichAndReformatFnTest.java
@@ -3,6 +3,9 @@ package com.hazelcast.jet.demos.traintrack;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.boot.configurationprocessor.json.JSONException;
@@ -12,22 +15,31 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class MyEnrichAndReformatFnTest {
+	
+	/* Beam 2.14.0 is first version for Jet BeamRunner
+	 * on http://repo1.maven.org/maven2/org/apache/beam/beam-runners-core-java/
+	 */
+	static long BEAM_2_14_RELEASE;
 
-	static final long NOW = 987654321;
-	
 	static MyEnrichAndReformatFn myEnrichAndReformatFn;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		myEnrichAndReformatFn = new MyEnrichAndReformatFn(NOW);
+		Date beamReleaseDate
+			= new SimpleDateFormat("yyyy-MM-dd HH:mm").parse("2019-07-24 22:06");
+
+		BEAM_2_14_RELEASE = beamReleaseDate.getTime();
+
+		myEnrichAndReformatFn = new MyEnrichAndReformatFn(BEAM_2_14_RELEASE);
 	}
 
 	@Test
 	public void json_parse() throws JSONException {
 		long offset = 789;
+		long timestampToUse = 987;
 		String input = "123,456," + offset;
 		
-		String output = myEnrichAndReformatFn._process(input);
+		String output = myEnrichAndReformatFn._makeJson(input, timestampToUse);
 		
 		log.info("{} -> {}", input, output);
 		
@@ -35,17 +47,29 @@ public class MyEnrichAndReformatFnTest {
 		
 		JSONObject jsonObject = new JSONObject(output);
 		
-		String latitude = jsonObject.getString("latitude");
-		assertThat("latitude", latitude, not(nullValue()));
-		assertThat("latitude", latitude, is(equalTo("123")));
+		String latitudeExtracted = jsonObject.getString("latitude");
+		assertThat("latitude", latitudeExtracted, not(nullValue()));
+		assertThat("latitude", latitudeExtracted, is(equalTo("123")));
+
+		String longitudeExtracted = jsonObject.getString("longitude");
+		assertThat("longitude", longitudeExtracted, not(nullValue()));
+		assertThat("longitude", longitudeExtracted, is(equalTo("456")));
 		
-		String longitude = jsonObject.getString("longitude");
-		assertThat("longitude", longitude, not(nullValue()));
-		assertThat("longitude", longitude, is(equalTo("456")));
+		String timestampExtracted = jsonObject.getString("timestamp");
+		assertThat("timestamp", timestampExtracted, not(nullValue()));
+		assertThat("timestamp", timestampExtracted, is(equalTo(""+timestampToUse)));
 		
-		String timestamp = jsonObject.getString("timestamp");
-		assertThat("timestamp", timestamp, not(nullValue()));
-		assertThat("timestamp", timestamp, is(equalTo(String.valueOf(NOW + offset * 1000))));
+		assertThat("timestampToUse", timestampToUse, not(equalTo(offset)));
 	}
 
+	@Test
+	public void calculate_timestamp() {
+		long offset = 789;
+
+		long timestampExpected = BEAM_2_14_RELEASE + offset;
+
+		long timestampCalculated = myEnrichAndReformatFn._calculateTimestamp(offset);
+
+		assertThat("timestampCalculated", timestampCalculated, is(equalTo(timestampExpected)));
+	}
 }
