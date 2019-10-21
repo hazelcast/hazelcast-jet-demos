39c891f9bb901761a4e6d477e4ffc18ed1cc334d
Neil Stevenson <neilstevenson@users.noreply.github.com>
2019-10-21 22:29:52 +0200

    Advance to 3.2

--- bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/job/MyPriceAccumulator.java
+++ bitcoin-death-cross/src/main/java/com/hazelcast/jet/demos/bitcoin/job/MyPriceAccumulator.java
@@ -7,6 +7,7 @@ import com.hazelcast.jet.datamodel.Tuple3;
 import com.hazelcast.jet.demos.bitcoin.domain.Price;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * <p>Merge two price streams, 50-point on the left
@@ -15,24 +16,86 @@ import lombok.Data;
  * observations the right stream (200-point) will have
  * no matching value.
  * </p>
- * <p>This is almost trivial enough to do with lambdas.
- * </p>
  */
 @Data
+@Slf4j
 public class MyPriceAccumulator {
 
 	private LocalDate date;
 	private BigDecimal left;
 	private BigDecimal right;
-	
+
+	/**
+	 * <p>Apply a new data from the 50-point stream into
+	 * the current accumulator.
+	 * </p>
+	 *
+	 * @param arg0 The 50-point stream
+	 * @return A trio combining 50-point and 200-point
+	 */
 	public MyPriceAccumulator setLeft(Price arg0) {
-		this.date = arg0.getLocalDate();
 		this.left = arg0.getRate();
+		if (this.date != null) {
+			if (!this.date.isEqual(arg0.getLocalDate())) {
+				log.error("Date clash: {} but setLeft({})", this, arg0);
+			}
+		} else {
+			this.date = arg0.getLocalDate();
+		}
 		return this;
 	}
 
+	/**
+	 * <p>Apply a new data from the 200-point stream into
+	 * the current accumulator.
+	 * </p>
+	 *
+	 * @param arg0 The 200-point stream
+	 * @return A trio combining 50-point and 200-point
+	 */
 	public MyPriceAccumulator setRight(Price arg0) {
 		this.right = arg0.getRate();
+		if (this.date != null) {
+			if (!this.date.isEqual(arg0.getLocalDate())) {
+				log.error("Date clash: {} but setRight({})", this, arg0);
+			}
+		} else {
+			this.date = arg0.getLocalDate();
+		}
+		return this;
+	}
+
+	/**
+	 * <p>Merge two accumulators together, eg. if accumulated on
+	 * another CPU. We do not expect the incoming side to overwrite
+	 * any data, so report error if so.
+	 * </p>
+	 *
+	 * @param that The other accumulator
+	 * @return A trio combining 50-point and 200-point
+	 */
+	public MyPriceAccumulator combine(MyPriceAccumulator that) {
+		if (this.date == null) {
+			this.date = that.getDate();
+		} else {
+			if (that.getDate() != null && !this.getDate().equals(that.getDate())) {
+				log.error("Date clash: {} but combine with ({})", this, that);
+			}
+		}
+		if (this.left == null) {
+			this.left = that.getLeft();
+		} else {
+			if (that.getLeft() != null && !this.getLeft().equals(that.getLeft())) {
+				log.error("Left clash: {} but combine with ({})", this, that);
+			}
+		}
+		if (this.right == null) {
+			this.right = that.getRight();
+		} else {
+			if (that.getLeft() != null && !this.getRight().equals(that.getRight())) {
+				log.error("Right clash: {} but combine with ({})", this, that);
+			}
+		}
 		return this;
 	}
 
